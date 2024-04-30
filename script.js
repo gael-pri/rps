17f95c53a152a142701e80577b9bc318dae71eab
Gaël <prigentgael@gmail.com>
2024-04-30 07:23:20 +0200

    Second version of RPS with DOM manipulation and eventListener !!

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
+
+const buttons = document.querySelectorAll('button');
+    let rps = '';
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            rps = button.id;
+            playGame();
+        });
+    });
+
 function getComputerChoice() {
     computerChoice =  Math.floor(Math.random() * 3);
     return computerChoice == 0 ? 'Rock' : computerChoice == 1 ? 'Paper' : 'Scissors';
 }
 
 function getPlayerSelection() {
-    let playerSelection = prompt("Rock ? Paper ? or Scissors ?");
+    let playerSelection = rps;
     choice = playerSelection.localeCompare("Rock", 'en', { sensitivity: 'base' }) == 0 ? 'Rock' : 
              playerSelection.localeCompare("Paper", 'en', { sensitivity: 'base' }) == 0 ? 'Paper' : 
              playerSelection.localeCompare("Scissors", 'en', { sensitivity: 'base' }) == 0 ? 'Scissors' : 'Go back school Mec !';
     return choice;
 }
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame() {
-    let score = 0;
-    numberOfRounds = 3;
-    for (i=0; i<numberOfRounds; i++) {
+    
         const playerSelection = getPlayerSelection();
         const computerSelection = getComputerChoice();
 
@@ -28,18 +38,27 @@ function playGame() {
                     (computerSelection == 'Rock' ? wInner : computerSelection == 'Paper' ?eQual : lOoser) : 
                     (computerSelection == 'Rock' ? lOoser : computerSelection == 'Paper' ? lOoser : eQual);
         
-        resultu = result == 0 ? 0 : result == 2 ? 0 : 1;
-        score += resultu;
+        result == 0 ? (playerResultu = 0, computerResultu = 0) : result == 1 ? (playerResultu = 1, computerResultu = 0) : (playerResultu = 0, computerResultu = 1);
+        playerScore += playerResultu;
+        computerScore += computerResultu;
 
         const deBut = 'With ' + playerSelection + ' on ' + computerSelection +  ' : ';
         const theeQual = 'nobody win and nobody loose';
         const thewInner = 'YOU WIN BABY !!';
         const thelOoser = 'YOU LOOSE MY POULE !!';
-        const round = "round" + i + " : " + result;
+        const round = result;
 
         resultat = result == 0 ? theeQual : result == 1 ? thewInner : thelOoser;
-        console.log(deBut + resultat + ' (' + round + ')');
-    }
-    final = score > (numberOfRounds / 2) ? "You win !!" : "You loose";
-    console.log(final);
+        match = deBut + resultat + ' (' + round + ')';
+        final = (playerScore == 5)  ? "You win !!" : (computerScore == 5) ? "You loose :-(" : 'Player : ' + playerScore + ' / Computer : ' + computerScore;
+
+        const body = document.querySelector("body");
+
+        const displayResult = document.createElement('div');
+        body.appendChild(displayResult);
+        displayResult.textContent = match;
+
+        const textFinal = document.createElement('p');
+        body.appendChild(textFinal);
+        textFinal.textContent = final;
 }
